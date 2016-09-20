4d387eae1571c376809d6583657a2c8276ed83bf
zeningjiang <zeningjiang@aa0508-73.labs.sce.carleton.ca>
2016-09-20 11:59:18 -0400

    modify of AddressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -3,16 +3,20 @@ import java.util.ArrayList;
 public class AddressBook {
 
 	private ArrayList<BuddyInfo> addressbook;
-	
-	public AddressBook(){
+
+	public AddressBook() {
 		addressbook = new ArrayList<BuddyInfo>();
 	}
-	
-	public void addBuddy(BuddyInfo bi){
-		addressbook.add(bi);
+
+	public void addBuddy(BuddyInfo bi) {
+		if (bi != null) {
+			addressbook.add(bi);
+		}
 	}
-	
-	public void removeBuddy(BuddyInfo bi){
-		addressbook.remove(bi);
+
+	public void removeBuddy(int index) {
+		if (index >= 0) {
+			addressbook.remove(index);
+		}
 	}
 }
